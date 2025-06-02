bf11bc1bd8e16a3e155401f86cfb6a6183b9a3c4
Nomeldnuor <147503885+Nomeldnuor@users.noreply.github.com>
2025-06-02 23:55:55 +0900

    Update vite.config.ts
    base 경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/dumbs_diary/', // ✅ 꼭 본인의 레포 이름으로 변경하세요
 })
