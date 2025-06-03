48f0ba0b282b1a1863d8025efd96d75af4a1e3bc
Nomeldnuor <147503885+Nomeldnuor@users.noreply.github.com>
2025-06-03 13:21:24 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   //base: '/dumbs_diary/', //needed when using github pages
-  base :'/'.
+  base :'/',
 })
